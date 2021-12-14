a095e210e415a4f005ababf621cd3599c83ea728
Olga.Chervyakova <o.chervyakova@mail.ru>
2021-12-14 11:35:24 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./style.css";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+
+
+
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Header />
+                ff
+
+            </div>
+        </Router>
+    );
+}
+
+export default App;
