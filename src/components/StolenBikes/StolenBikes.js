d06054537ea2ed96bf84f45030482958cae942a6
Olga.Chervyakova <o.chervyakova@mail.ru>
2022-03-18 12:36:06 +0700

    nm

--- src/components/StolenBikes/StolenBikes.js
+++ src/components/StolenBikes/StolenBikes.js
@@ -11,28 +11,23 @@ const StolenBikes = () => {
     const handleDelete = e => {
         e.preventDefault();
 
-        const itemIdx = +e.target.attributes.getNamedItem("deteleitem").value;
+        const itemIdx = + e.target.attributes.getNamedItem("deteleitem").value;
         const item = data[itemIdx];
-        console.log(itemIdx)
     }
     const loadData = async () => {
         const response = await axios.get("https://sf-final-project.herokuapp.com/api/cases", {
             headers:{
-                Authorization: "Bearer " + localStorage.getItem
+                Authorization: `Bearer ${localStorage.getItem("bikeTheftAuthorization")}`
             }
-        });
-        //.then(res => {
-        console.log(response.data);
-        setData(response.data);
-         //});
+        }).then(res => {
+            setData(res.data.data);
+         });
     };
 
     useEffect(() => {
         loadData();
     }, []);
-
-    return (
-        <div className="main-container">
+    return (<div className="main-container">
             <div className="table-container">
                 <div className="tbl-header">
                     <table cellPadding="0" cellSpacing="0" border="0">
@@ -51,27 +46,29 @@ const StolenBikes = () => {
                 </div>
                 <div className="tbl-content">
                     <table>
-                        <tmain>
-                        {data.map((report, index) => {
-                            return (
-                                <tr key={index + 1}>
-                                    <td>{Moment(report.date).format("DD.MM.YYYY")}</td>
-                                    <td>{report.type}</td>
-                                    <td>{report.color}</td>
-                                    <td>{report.ownerFullName}</td>
-                                    <td>{report.officers || "-"}</td>
-                                    <td>{report.status}</td>
-                                    <td>{Moment(report.updateAt).format("DD.MM.YYYY")}</td>
-                                    <td>{report.resolution}</td>
-                                    <td>
-                                        <button deteleitem={index} onClick={handleDelete}>
-                                            Delete
-                                        </button>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                        </tmain>
+                        <tbody>
+                        {
+                            data.map((report, index) => {
+                                return (
+                                    <tr key={index + 1}>
+                                        <td>{Moment(report.date).format("DD.MM.YYYY")}</td>
+                                        <td>{report.type}</td>
+                                        <td>{report.color}</td>
+                                        <td>{report.ownerFullName}</td>
+                                        <td>{report.officers || "-"}</td>
+                                        <td>{report.status}</td>
+                                        <td>{Moment(report.updateAt).format("DD.MM.YYYY")}</td>
+                                        <td>{report.resolution}</td>
+                                        <td>
+                                            <button deteleitem={index} onClick={handleDelete}>
+                                                Delete
+                                            </button>
+                                        </td>
+                                    </tr>
+                                );
+                            })
+                        }
+                        </tbody>
                     </table>
                 </div>
             </div>
